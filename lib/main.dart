4b9aa0d064053d94a844371a2c79d02cf0a3f6bb
VinithaRaj <vini97bluestar@gmail.com>
2022-01-11 16:52:05 +0800

    different font

--- lib/main.dart
+++ lib/main.dart
@@ -191,8 +191,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   alignment: Alignment.center,
 
                   child: ListTile(
-                    title: Text(currentTitle+" Registration", style: GoogleFonts.cinzel(fontWeight: FontWeight.bold, fontSize: 20),),
-                    subtitle: Text(currentDate, style: GoogleFonts.cinzel( fontSize: 18,color: Colors.black45,fontWeight: FontWeight.bold),),
+                    title: Text(currentTitle+" Registration", style: GoogleFonts.cormorantGaramond(fontWeight: FontWeight.bold, fontSize: 20),),
+                    subtitle: Text(currentDate, style: GoogleFonts.cormorantGaramond( fontSize: 18,color: Colors.black45,fontWeight: FontWeight.bold),),
                   ),
                 ),
 
@@ -211,7 +211,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 width: _width/1.2,
                 alignment: Alignment.topLeft,
                 child: Container(
-                  child: Text("Enter Name: (1 per person)", style: GoogleFonts.cinzel(fontWeight: FontWeight.bold),),
+                  child: Text("Enter Name: (1 per person)", style: GoogleFonts.cormorantGaramond(fontWeight: FontWeight.bold),),
                 ),
               ),
 
